11086bbe8319cf4d358603eb031c935fc9406a36
Ryan <Rylang.dev@gmail.com>
2019-02-28 20:58:57 -0600

    Added more functionality

--- main.lua
+++ main.lua
@@ -1,1 +1,29 @@
-message("Hello World of Warcraft")
+-- Login Notification
+-- first initialize a Frame object
+local efLogin = CreateFrame("Frame")
+
+-- Then register that frame to an event of your choice
+efLogin:RegisterEvent("PLAYER_LOGIN")
+
+-- Then set a script that is executed when the Event occurs
+efLogin:SetScript("OnEvent", function(self, event, ...)
+    ChatFrame1:AddMessage('Why Hello There '.. UnitName("Player"))
+end)
+
+-- Combat Notification
+local efCombat = CreateFrame("Frame")
+efCombat:RegisterEvent("PLAYER_REGEN_DISABLED")
+efCombat:SetScript("OnEvent", function(self, event, ...)
+    ChatFrame1:AddMessage('You are now entering combat! Kill it!')
+end)
+
+-- Rest Notification
+local efRest = CreateFrame("Frame")
+efRest:RegisterEvent("PLAYER_UPDATE_RESTING")
+efRest:SetScript("OnEvent", function(self, event)
+    if IsResting() then
+        ChatFrame1:AddMessage('You entered a rest area.  Gaining Bonus XP!')
+    else
+        ChatFrame1:AddMessage('You left a rest area.  No longer gaining bonus XP!')
+    end    
+end)
